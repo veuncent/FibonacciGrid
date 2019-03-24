de7373e74a9a168d3f70d6752f068d7595504a67
Vincent Meijer <meijer.vincent@gmail.com>
2019-03-24 16:37:09 +0100

    #1 Add Grid and Cell models. Add additional GridList model for performance benchmarking (list vs array).

---
+++ FibonacciGrid/FibonacciGrid.Client/Models/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace FibonacciGrid.Client.Models
+{
+    public class Grid
+    {
+        public GridCell[,] FibonacciGrid { get; }
+
+        public Grid(int gridSize = 50)
+        {
+            FibonacciGrid = new GridCell[gridSize, gridSize];
+
+            for (var i = 0; i < gridSize; i++)
+            {
+                for (var j = 0; j < gridSize; j++)
+                {
+                    FibonacciGrid[i, j] = new GridCell();
+                }
+            }
+        }
+    }
+
+    public class GridList
+    {
+        public List<List<GridCell>> FibonacciGrid { get; }
+
+        public GridList(int gridSize = 50)
+        {
+            FibonacciGrid = new List<List<GridCell>>(50);
+
+            for (var i = 0; i < gridSize; i++)
+            {
+                FibonacciGrid.Add(new List<GridCell>(50));
+
+                for (var j = 0; j < gridSize; j++)
+                {
+                    FibonacciGrid[i].Add(new GridCell());
+                }
+            }
+        }
+    }
+}
---
+++ FibonacciGrid/FibonacciGrid.Client/Models/GridCell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FibonacciGrid.Client.Models
+{
+    public class GridCell
+    {
+        public int Value { get; set; } = 0;
+
+        public bool IsFibonacci { get; }
+    }
+}
