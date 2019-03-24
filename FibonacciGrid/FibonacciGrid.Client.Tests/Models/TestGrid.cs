797bbb60e08c2212975a4dda485d40b9d3697d9a
Vincent Meijer <meijer.vincent@gmail.com>
2019-03-24 16:37:36 +0100

    #1 Add unit test project. Add unit test for Grid constructor + performance benchmarks

---
+++ FibonacciGrid/FibonacciGrid.Client.Tests/Models/TestGrid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FibonacciGrid.Client.Models;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace FibonacciGrid.Client.Tests.Models
+{
+    public class TestGrid
+    {
+        [Test]
+        public void Given_Grid_When_ConstructingObject_Expect_InitializedTwoDimensionalArray()
+        {
+            // Act
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var grid = new Grid(50);
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.Elapsed);
+
+            stopwatch.Start();
+            var gridList = new GridList(50);
+            stopwatch.Stop();
+
+            Console.WriteLine(stopwatch.Elapsed);
+
+            // Assert
+            Assert.AreEqual(50, grid.FibonacciGrid.GetLength(0));
+            Assert.AreEqual(50, grid.FibonacciGrid.GetLength(1));
+
+            var item = grid.FibonacciGrid[49, 49];
+            Assert.IsNotNull(item);
+            Assert.AreEqual(typeof(GridCell), item.GetType());
+        }
+    }
+}
