62f4dd304cf7f9dee1de8afa54a317e368fb5782
Vincent Meijer <meijer.vincent@gmail.com>
2019-03-24 20:16:52 +0100

    #3 Add service that checks if number if Fibonacci. Add unit tests

---
+++ FibonacciGrid/FibonacciGrid.Client.Tests/Services/TestFibonacciCheckerService.cs
@@ -0,0 +1,36 @@
+﻿using FibonacciGrid.Client.Services;
+using NUnit.Framework;
+
+namespace FibonacciGrid.Client.Tests.Services
+{
+    public class TestFibonacciCheckerService
+    {
+        [Test]
+        public void Given_FibonacciNumber_When_CheckingIfIsFibonacciNumber_Expect_True()
+        {
+            // Arrange
+            var fibonacciCheckerService = new FibonacciCheckerService();
+            const int fibonacciNumber = 3;
+
+            // Act
+            var isFibonacci = fibonacciCheckerService.IsFibonacci(fibonacciNumber);
+
+            // Assert
+            Assert.True(isFibonacci);
+        }
+
+        [Test]
+        public void Given_NonFibonacciNumber_When_CheckingIfIsFibonacciNumber_Expect_False()
+        {
+            // Arrange
+            var fibonacciCheckerService = new FibonacciCheckerService();
+            const int fibonacciNumber = 4;
+
+            // Act
+            var isFibonacci = fibonacciCheckerService.IsFibonacci(fibonacciNumber);
+
+            // Assert
+            Assert.False(isFibonacci);
+        }
+    }
+}
---
+++ FibonacciGrid/FibonacciGrid.Client/Services/FibonacciCheckerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FibonacciGrid.Client.Services
+{
+    public class FibonacciCheckerService
+    {
+        public bool IsFibonacci(int number)
+        {
+            var fiveNSquare = 5 * number * number;
+            return IsPerfectSquare(fiveNSquare - 4) || IsPerfectSquare(fiveNSquare + 4);
+        }
+
+        private static bool IsPerfectSquare(int number)
+        {
+            var squareRoot = (int) Math.Sqrt(number);
+            return (squareRoot * squareRoot == number);
+        }
+    }
+}
