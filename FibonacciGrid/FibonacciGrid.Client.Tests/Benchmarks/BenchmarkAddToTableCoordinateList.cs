b4cb152e39f95ced4266d7b170ca4674ab34f069
Vincent Meijer <meijer.vincent@gmail.com>
2019-03-31 14:44:40 +0200

    Add benchmark tests

---
+++ FibonacciGrid/FibonacciGrid.Client.Tests/Benchmarks/BenchmarkAddToTableCoordinateList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using FibonacciGrid.Client.Models;
+using FibonacciGrid.Client.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace FibonacciGrid.Client.Tests.Benchmarks
+{
+    public class BenchmarkAddToTableCoordinateList
+    {
+        [Test, Category("Benchmark")]
+        public void BenchmarkListTuplePerformance()
+        {
+            var fibonacciCheckerService = Mock.Of<IFibonacciCheckerService>();
+            Mock.Get(fibonacciCheckerService).Setup(service => service.IsFibonacci(It.IsAny<int>())).Returns(true);
+            var grid = new Grid(fibonacciCheckerService, 50);
+
+            var listTuple = new List<Tuple<int, int>>();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (var x = 0; x < 100; x++)
+            {
+                for (var i = 0; i < grid.FibonacciGrid.GetLength(0); i++)
+                {
+                    for (var j = 0; j < grid.FibonacciGrid.GetLength(1); j++)
+                    {
+                        listTuple.Add(Tuple.Create(i, j));
+                    }
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        [Test, Category("Benchmark")]
+        public void BenchmarkListListPerformance()
+        {
+            var fibonacciCheckerService = Mock.Of<IFibonacciCheckerService>();
+            Mock.Get(fibonacciCheckerService).Setup(service => service.IsFibonacci(It.IsAny<int>())).Returns(true);
+            var grid = new Grid(fibonacciCheckerService, 50);
+
+            var listList = new List<List<int>>();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (var x = 0; x < 100; x++)
+            {
+                for (var i = 0; i < grid.FibonacciGrid.GetLength(0); i++)
+                {
+                    for (var j = 0; j < grid.FibonacciGrid.GetLength(1); j++)
+                    {
+                        listList.Add(new List<int> { i, j });
+                    }
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+
+        [Test, Category("Benchmark")]
+        public void BenchmarkListArrayPerformance()
+        {
+            var fibonacciCheckerService = Mock.Of<IFibonacciCheckerService>();
+            Mock.Get(fibonacciCheckerService).Setup(service => service.IsFibonacci(It.IsAny<int>())).Returns(true);
+            var grid = new Grid(fibonacciCheckerService, 50);
+
+            var listArray = new List<int[]>();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (var x = 0; x < 100; x++)
+            {
+                for (var i = 0; i < grid.FibonacciGrid.GetLength(0); i++)
+                {
+                    for (var j = 0; j < grid.FibonacciGrid.GetLength(1); j++)
+                    {
+                        listArray.Add(new[] { i, j });
+                    }
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
